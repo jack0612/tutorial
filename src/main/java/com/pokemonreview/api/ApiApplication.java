b01eac4266b1842b01de7e055fd9fa196469dd24
jack.lu <jack.lu@innovapost.com>
2023-07-08 21:23:37 -0400

    add swagger2

--- src/main/java/com/pokemonreview/api/ApiApplication.java
+++ src/main/java/com/pokemonreview/api/ApiApplication.java
@@ -2,8 +2,13 @@ package com.pokemonreview.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableSwagger2
 public class ApiApplication {
 
 	public static void main(String[] args) {
